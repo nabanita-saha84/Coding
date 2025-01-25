9a367dd805f0b82e8cd79e7b8a725f9bb0731724
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-01-25 18:53:42 +0530

    Time: 1 ms (16.16%), Space: 43.1 MB (55.36%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length-1;
+        int i=n;
+
+        while(i>0 && nums[i]<=nums[i-1]){
+            i--;
+        }
+
+        if(i==0){
+            Arrays.sort(nums);
+            return;
+        }
+        else{
+            int t = nums[i-1];
+            int nearest=i;
+            for(int j=i; j<=n; j++){
+                if(nums[j]>t && nums[j]<nums[nearest]){
+                    nearest = j;
+                }
+            }
+            nums[i-1] = nums[nearest];
+            nums[nearest] = t;
+            Arrays.sort(nums, i, n+1);
+        }
+    }
+}
