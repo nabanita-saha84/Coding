f5523c6ea27fdd8e42411a884abb46ed9b4d4a39
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-01-28 13:58:16 +0530

    Time: 8 ms (2.61%), Space: 44.5 MB (7.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,16 +1,18 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-
-        int top_ind=0, right_ind=n-1, bottom_ind=n-1, left_ind=0;
-        for(int i=0; i<n/2; i++){
-            for(int j=i; j<n-1-i; j++){
-                int temp = matrix[top_ind+i][j];
-                matrix[top_ind+i][j] = matrix[n-1-j][left_ind+i];
-                matrix[n-1-j][left_ind+i] = matrix[bottom_ind-i][n-1-j];
-                matrix[bottom_ind-i][n-1-j] = matrix[j][right_ind-i];
-                matrix[j][right_ind-i] = temp;
+        int[][] resultant = new int[n][n];
+        int i=n-1;
+        for(int[] row : matrix){
+            for(int j=0; j<n; j++){
+                resultant[j][i] = row[j];
             }
+            i--;            
+            System.out.println(Arrays.deepToString(resultant));
+        }       
+        i=0;
+        for(int[] row : resultant){
+            matrix[i++] = row;
         }
     }
 }
