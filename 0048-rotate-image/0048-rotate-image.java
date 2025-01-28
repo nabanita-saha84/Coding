308d84048fe55a18592421a008d4ab62d42e852b
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-01-28 13:54:23 +0530

    Time: 0 ms (100%), Space: 42.3 MB (40.75%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -5,18 +5,12 @@ class Solution {
         int top_ind=0, right_ind=n-1, bottom_ind=n-1, left_ind=0;
         for(int i=0; i<n/2; i++){
             for(int j=i; j<n-1-i; j++){
-                int top = matrix[top_ind+i][j];
-                int right = matrix[j][right_ind-i];
-                int bottom = matrix[bottom_ind-i][n-1-j];
-                int left = matrix[n-1-j][left_ind+i];
-                matrix[top_ind+i][j] = left;
-                matrix[j][right_ind-i] = top;
-                matrix[bottom_ind-i][n-1-j] = right;
-                matrix[n-1-j][left_ind+i] = bottom;
-                System.out.println("Top : "+top+" Right : "+right+" Bottom : "+bottom+" Left  : "+left);
-                System.out.println(Arrays.deepToString(matrix));
+                int temp = matrix[top_ind+i][j];
+                matrix[top_ind+i][j] = matrix[n-1-j][left_ind+i];
+                matrix[n-1-j][left_ind+i] = matrix[bottom_ind-i][n-1-j];
+                matrix[bottom_ind-i][n-1-j] = matrix[j][right_ind-i];
+                matrix[j][right_ind-i] = temp;
             }
         }
-
     }
 }
