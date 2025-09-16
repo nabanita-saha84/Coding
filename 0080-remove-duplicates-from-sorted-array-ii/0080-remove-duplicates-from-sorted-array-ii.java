0ccc487515077b27ba0aadf2c28a6ebdf45bc94f
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-09-16 15:51:52 +0530

    Time: 0 ms (100%), Space: 47 MB (9.55%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -5,16 +5,15 @@ class Solution {
         int i=0, j=0;
         int n = nums.length;
         for(; j<n; j++){
-            if(nums[i]!=nums[j] && count>=2){
-                nums[i+1]=nums[i];
-                i+=2;
-                nums[i] = nums[j];
-                count=1;
-            } else if(nums[i]!=nums[j] && count<2){
+            if(nums[i]!=nums[j]){
+                if(count>=2) {
+                    nums[i+1]=nums[i];
+                    i++;
+                }
                 i++;
                 nums[i]= nums[j];
-            }
-            else if(nums[i]==nums[j]){
+                count=1;
+            } else if(nums[i]==nums[j]){
                 count++;
             }
         }    
