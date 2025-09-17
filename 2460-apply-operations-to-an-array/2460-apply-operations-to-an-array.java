c99201f325a764ef6fa9f374b7a80db7c7deaad9
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-09-17 07:12:27 +0530

    Time: 1 ms (93.43%), Space: 43.9 MB (42.03%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -6,24 +6,16 @@ class Solution {
                 nums[i+1]=0;
             }
         }
-
-        for(int i=0, j=nums.length-1; i<=j; i++){
-            if(nums[i]==0){
-                while(nums[j]==0){
-                    j--;
-                }
-                if(j>i){
-                    swap(i, j, nums);
-                    j--;
-                }
+        int[] arr = new int[nums.length];
+        int k=0;
+        for(int i: nums){
+            if(i!=0){
+                arr[k++]=i;
             }
         }
-        return nums;
-    }
-
-    void swap(int a, int b, int[] arr){
-        int t = arr[a];
-        arr[a] = arr[b];
-        arr[b] = t;
+        // for(; k<nums.length; k++){
+        //     arr[k]=0;
+        // }
+        return arr;
     }
 }
