27ce40307d0a18c36694fc66c92b91a1cfd4cc5a
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-11-15 17:22:44 +0530

    Time: 11 ms (94.2%), Space: 48 MB (17.39%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,50 +1,39 @@
 class Solution {
     public int calculate(String s) {
-        Deque<Integer> numbers = new ArrayDeque<>();
-        s = s.replaceAll(" ", "");
-        int result=0;
-        int sign=1;
         int len = s.length();
-        for(int i=0; i<len; i++){
-            int curr_num=0;
-            char c = s.charAt(i);
-            if(Character.isDigit(c)){
-                int[] res = calculateNumber(s, i);
-                curr_num = res[0];
-                i=res[1];
-
-                numbers.push(curr_num*sign);
-            } else if( c == '+'){
-                sign = 1;
-            } else if( c == '-'){
-                sign = -1;
-            } else if( c == '*'){
-                int[] res = calculateNumber(s, i+1);
-                curr_num = res[0];
-                i=res[1];
-                numbers.push(numbers.pop() * curr_num);
-            } else if ( c == '/'){
-                int[] res = calculateNumber(s, i+1);
-                curr_num = res[0];
-                i=res[1];
-                numbers.push(numbers.pop() / curr_num);
+        
+        if (len == 0)
+        return 0;
+        Deque<Integer> results = new ArrayDeque<>();
+        char sign = '+';
+        int token = 0, n = s.length();
+        for(int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(ch == ' ' && i != n - 1) {
+                continue;
             }
+            if('0' <= ch && ch <= '9') {
+                token = token * 10 + ch - '0';
+                if(i != n - 1) {
+                    continue;
+                }
+            }
+            if(sign == '+') {
+                results.push(token);
+            } else if(sign == '-') {
+                results.push(-token);
+            } else if(sign == '*') {
+                results.push(results.pop() * token);
+            } else { // sign is /
+                results.push(results.pop() / token);
+            }
+            token = 0;
+            sign = ch;
         }
-
-        while(!numbers.isEmpty()){
-            result+=numbers.pop();
+        int res = 0;
+        while(results.size() > 0) {
+            res += results.pop();
         }
-        return result;
+        return res;
     }
-
-    int[] calculateNumber(String s, int initial_index){
-        int index = initial_index;
-        int num=0;
-        while(index<s.length() && Character.isDigit(s.charAt(index))){
-            num = num*10  +    s.charAt(index)-'0';
-            index++;
-        }
-        return new int[]{num, index-1};
-    }
-
 }
