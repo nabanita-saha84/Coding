11f285478e880b779f79ab655fb3f0bb8cad13e8
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-11-18 14:04:22 +0530

    Time: 6 ms (9.53%), Space: 42.9 MB (7.31%) - LeetHub

---
+++ 0504-base-7/0504-base-7dupdup.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if(num==0)  return "0";
+
+        String base7 = "";
+        boolean neg=false;
+        if(num<0)   neg=true;
+        num = Math.abs(num);
+        while(num!=0){
+            base7 = (num%7) + base7;
+            num /= 7;
+        }
+
+        return neg?("-"+base7):base7;
+    }
+}
