a8c5841c6b88a44b3262a1b0e802b0bfe862623b
nabanita-saha84 <58902270+nabanita-saha84@users.noreply.github.com>
2025-11-26 22:19:24 +0530

    Time: 0 ms (100%), Space: 61 MB (30.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,33 +1,19 @@
 class Solution {
-    //Sol with rotating one at a time O(n*k)
     public void rotate(int[] nums, int k) {
-        int n= nums.length;
-        k = k%n;
-        for(int i=0; i<k; i++){
-            int lastValue = nums[n-1];
-            for(int j=n-2; j>=0; j--){
-                nums[j+1] = nums[j];
-            }
-            nums[0] = lastValue;
+        int len = nums.length;
+        k = k%len;
+        len--;
+        rotate(nums,0,len-k);
+        rotate(nums, len-k+1, len);
+        rotate(nums, 0, len);
+
+    }
+    public void rotate(int[] nums, int l, int r){
+        while(l<r){
+            int t = nums[l];
+            nums[l] = nums[r];
+            nums[r] = t;
+            l++; r--;
         }
     }
-
-
-    // Solution with k extra space
-    // public void rotate(int[] nums, int k) {
-    //     k = k%nums.length;
-    //     if(k==0 || nums.length==0 || nums.length==1){
-    //         return;
-    //     }
-    //     int[] temp = new int[k];
-    //     for(int i=0; i<k; i++){
-    //         temp[i] = nums[nums.length-1-i];
-    //     }
-    //     for(int i=nums.length-k-1; i>=0; i--){
-    //         nums[i+k] = nums[i];
-    //     }
-    //     for(int i=0; i<k; i++){
-    //         nums[i] = temp[k-1-i];
-    //     }
-    // }
 }
